1bd08227a9c557aae07690f46726af30eca0672c
lamst <lam.stanley@gmail.com>
2022-07-03 13:23:26 +0000

    Finished Lab2 exercise 4

---
+++ Lab2Exercise4.java
@@ -0,0 +1,15 @@
+/**
+ * This class demonstrates the use of conditional (if) statements
+ * @author Jennifer Lau
+ */
+class Lab2Exercise4
+{
+	public static void main(String[] args)
+	{
+    String firstName = "Jennifer";
+    String familyName = "Lau";
+    String fullName = firstName + " " + familyName;
+
+    System.out.print(fullName);
+  }
+}
