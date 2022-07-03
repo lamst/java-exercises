6c8bdfeaee14cdb909af5a782cb96b1958b6a88d
lamst <lam.stanley@gmail.com>
2022-07-03 09:36:21 +0000

    Completed exercise 2

---
+++ Exercise2.java
@@ -0,0 +1,18 @@
+/**
+*@author Jennifer Lau
+*/
+class Lab2Exercise2
+{
+	public static void main(String[] args)
+	{
+		// Integer variable
+    int number = 9;
+    int remainder = number % 2;
+
+    // If remainder 
+    if (remainder == 0)
+      System.out.println("even number");
+    else
+      System.out.println("odd number");
+  }
+}
